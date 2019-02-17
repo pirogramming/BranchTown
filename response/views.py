d96551cdca124d443af4ef674eba4556cf202668
gyeong707 <kang9260@naver.com>
2019-02-17 13:48:29 +0900

    chart 수정중

--- response/views.py
+++ response/views.py
@@ -5,12 +5,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth import get_user_model
+from survey.models import Survey, Field, MultipleChoice
 
 User = get_user_model()
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'response/charts.html')
+        survey = Survey.objects.get(pk=1)
+        field = Field.objects.filter(survey=survey)
+        return render(request, 'response/charts.html', {
+            "fields": field,
+        })
 
 
 def get_data(request, *args, **kwargs):
@@ -26,11 +31,23 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        qs_count = User.objects.all().count()
-        labels = ["1", "2", "3", "4"]
-        default_items = [qs_count, 1234, 4455, 5000]
+
+        fields = Survey.objects.get(pk=1).field_set.all()
+
+        labels = []
+        default_items = []
+
+        choices = fields[0].multiplechoice_set.all()
+        choices_len = choices.count()
+
+        for choice in choices:
+            labels.append(choice.choice_text)
+            default_items.append(choice.votes)
+
+
         data = {
             "labels": labels,
             "default": default_items,
         }
+
         return Response(data)
