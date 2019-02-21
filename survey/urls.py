8668d94886e19cb956d9479daed00c0abbaff6fb
choiyeonseok <smr603@snu.ac.kr>
2019-02-21 18:02:37 +0900

    merge성공

--- survey/urls.py
+++ survey/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "survey"
 
 urlpatterns = [
+    path('make/', views.make_survey, name='make_survey'),
+    path('make/<int:pk>/', views.make_index, name='make_index'),
     path('make/<int:pk>/choice/', views.make_multiple_choice, name='make_multiple_choice'),
     path('make/<int:pk>/text/', views.make_text_answer, name='make_text_answer'),
     # path('<int:pk>/multiple_choice/', views.multiple_choice, name='multiple_choice'),
